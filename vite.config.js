6ccbee9780a395b785cda57072f692c783424db5
MazzomesClone <49418843+MazzomesClone@users.noreply.github.com>
2023-05-04 17:32:59 +1200

    updated vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,6 @@ export default ({ mode }) => {
   return defineConfig({
     plugins: [react()],
     server: {
-      middleware: {
-        headers: {
-          'Cache-Control': 'no-cache, no-store, must-revalidate',
-          'Pragma': 'no-cache',
-          'Expires': '0'
-        }
-      },
       proxy: {
         '/api': {
           target: API_URL,
